285a49c787714281d1a578e72c68e23f393bd4a6
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-18 20:32:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["Dino" , "Noni" , "Kat", "Ken"]
   element = "Cami"
-  class.push element
+  class << element
 end
 
 def add_element_to_start_of_array(array, element)
