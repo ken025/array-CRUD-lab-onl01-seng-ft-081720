f5425ba5a71f37ccd8b11c89ee238703ccfa77b4
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-18 20:34:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,10 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Dino" , "Noni" , "Kat", "Ken"]
-  element = "arrays!"
-  class.push element
-end
+  array.push element
 end 
 
 def add_element_to_start_of_array(array, element)
