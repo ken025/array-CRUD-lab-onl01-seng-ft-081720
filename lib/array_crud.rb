7001206db221a86a4e00c502b0032638ae5fc06e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-18 20:27:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  class["Dino" , "Noni" , "Kat", "Ken"]
+  ["Dino" , "Noni" , "Kat", "Ken"]
 end
 
 def add_element_to_end_of_array(array, element)
